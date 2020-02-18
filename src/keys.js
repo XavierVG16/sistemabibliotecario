cf3e495303951fa56ea196fadddd232d7781f500
XavierVG <kjviervelasque16@gmail.com>
2020-02-18 16:24:05 -0500

    nuevo servidor de base de datos

--- src/keys.js
+++ src/keys.js
@@ -1,36 +1,37 @@
 module.exports = {
 
     database: {
-       
-        host: 'sql10.freesqldatabase.com',
-        user: 'sql10322070',
-        password: '4QSdBKyUr3',
-        database: 'sql10322070'
-       
 
-        
+        host: 'sql10.freesqldatabase.com',
+        user: 'sql10323293',
+        password: 'Fv36q5NRza',
+        database: 'sql10323293'
+       
 
  /*
-  host: 'localhost',
+
+        
+       
+
+
+        host: 'localhost',
         user: 'root',
         password: '',
         database: 'sistemabiblioteca'
 
-       
-
         
-        host: 'bntbwjjaofujbpft68nx-mysql.services.clever-cloud.com',
-        user: 'u4hfcba98ean319n',
-        password: 'ELdM8VCna9tWdgyXhaJe',
-        database: 'bntbwjjaofujbpft68nx'
+       
         '
 
-        
+           host: 'bntbwjjaofujbpft68nx-mysql.services.clever-cloud.com',
+       user: 'u4hfcba98ean319n',
+       password: 'ELdM8VCna9tWdgyXhaJe',
+       database: 'bntbwjjaofujbpft68nx'
 
 
         */
        
- 
+    
      
     }
 
