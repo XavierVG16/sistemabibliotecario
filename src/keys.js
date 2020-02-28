275d14279672c23fe56e9973a105d9257199a98c
XavierVG <kjviervelasque16@gmail.com>
2020-02-28 18:32:13 -0500

    Base de datos en Clever Cloud

--- src/keys.js
+++ src/keys.js
@@ -1,37 +1,41 @@
 module.exports = {
 
     database: {
+      
+     
+
+ /*  host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'sistemabiblioteca'
+
 
         host: 'sql10.freesqldatabase.com',
         user: 'sql10323293',
         password: 'Fv36q5NRza',
         database: 'sql10323293'
+
+          host: 'bhpjepiev3h1lnhkdaws-mysql.services.clever-cloud.com',
+        user: 'uxm1civdjdgh9dby',
+        password: 'g0aNZhEzqXwt5pIayJki',
+        database: 'bhpjepiev3h1lnhkdaws'
        
 
- /*
-
+     
         
-       
 
-
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'sistemabiblioteca'
-
-        
        
         '
 
-           host: 'bntbwjjaofujbpft68nx-mysql.services.clever-cloud.com',
-       user: 'u4hfcba98ean319n',
-       password: 'ELdM8VCna9tWdgyXhaJe',
-       database: 'bntbwjjaofujbpft68nx'
+        
 
 
         */
        
-    
+       host: 'bhpjepiev3h1lnhkdaws-mysql.services.clever-cloud.com',
+       user: 'uxm1civdjdgh9dby',
+       password: 'g0aNZhEzqXwt5pIayJki',
+       database: 'bhpjepiev3h1lnhkdaws'
      
     }
 
