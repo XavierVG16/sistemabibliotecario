7b963ac37e2c660a8eb8f2d0bb24659c1cb6175f
XavierVG <kjviervelasque16@gmail.com>
2020-06-13 21:34:19 -0500

    adcicion de m odulo de reportes de prestamos

--- src/keys.js
+++ src/keys.js
@@ -3,19 +3,19 @@ module.exports = {
     database: {
       
        /*
-
-       host: 'bhpjepiev3h1lnhkdaws-mysql.services.clever-cloud.com',
-    user: 'uxm1civdjdgh9dby',
-    password: 'g0aNZhEzqXwt5pIayJki',
-    database: 'bhpjepiev3h1lnhkdaws'
-  
-     */
- 
-      
     host: 'localhost',
     user: 'root',
     password: '',
     database: 'sistemabiblioteca'
+      
+     */
+ 
+    host: 'bhpjepiev3h1lnhkdaws-mysql.services.clever-cloud.com',
+    user: 'uxm1civdjdgh9dby',
+    password: 'g0aNZhEzqXwt5pIayJki',
+    database: 'bhpjepiev3h1lnhkdaws'
+  
+
 
     }
 
